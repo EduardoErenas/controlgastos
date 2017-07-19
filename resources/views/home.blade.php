9b4833c8da54a123298e2d2b6b260b99b0c39f7b
Nathanael Martinez R <jazlucnata@gmail.com>
2017-07-19 11:59:43 -0600

    actualizando dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('master')
 
 @section('titulo')
-<div class="callout callout-info">
-    <div class="content-header" style="padding-top: 0px !important">
-      <h1>General <small></small></h1>
+
+    <div class="" ">
+     <h3 class="text-center" style="background: rgba(120,47,85,0.6);padding: 10px; border-radius: 3px;">GENERAL</h3>
     </div> 
-  </div>
+  
 <ol class="breadcrumb">
     <li><a href="#"><i class="fa fa-dashboard"></i> Principal</a></li>
     <li class="active">Home</li>
@@ -14,7 +14,7 @@
 
 @section('contenido')
 	<div class="row">
-        <div class="col-lg-3 col-xs-12 col-sm-3">
+        <div class="col-lg-4 col-xs-12 col-sm-4">
           <!-- small box -->
           <div class="small-box bg-aqua">
             <div class="inner">
@@ -27,7 +27,7 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-xs-12 col-sm-3">
+        <div class="col-lg-4 col-xs-12 col-sm-4">
           <!-- small box -->
           <div class="small-box bg-green">
             <div class="inner">
@@ -40,7 +40,7 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-xs-12 col-sm-3">
+        <div class="col-lg-4 col-xs-12 col-sm-4">
           <!-- small box -->
           <div class="small-box bg-yellow">
             <div class="inner">
@@ -53,26 +53,12 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-xs-12 col-sm-3">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>Reportes</h3>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-           <a href="{{url('/reportes')}}" class="small-box-footer">Ver Reporte<i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
+        
       </div>
       
       <div class="row">
-        <div class="col-lg-6">
-          
-        </div>
-        <div class="col-lg-3">
+
+        <div class="col-lg-3 col-sm-6 col-xs-12">
             <div class="info-box bg-red">
             <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
 
@@ -84,7 +70,7 @@
         </div>
         
         </div>
-        <div class="col-lg-3">
+        <div class="col-lg-3 col-sm-6 col-xs-12">
           <div class="info-box bg-green">
             <span class="info-box-icon"><i class="ion ion-ios-heart-outline"></i></span>
             <div class="info-box-content">
