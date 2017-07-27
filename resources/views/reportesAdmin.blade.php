fbcee9067a206e3bec189d5a2be1fa054d750e98
eduardo14 <eduardo0091400914@gmail.com>
2017-07-27 10:43:12 -0600

    vista reporte modificacion graficas

--- resources/views/reportesAdmin.blade.php
+++ resources/views/reportesAdmin.blade.php
@@ -57,63 +57,6 @@
           <!-- LINE CHART -->
          
         </div>
-        <div class="col-md-6">
-          <div class="box box-danger">
-            <div class="box-header with-border">
-              <h3 class="box-title">Ingresos con categorias</h3>
-
-              <div class="box-tools pull-right">
-                <!--
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>-->
-              </div>
-            </div>
-            <div class="box-body">
-              <canvas id="pieChart" style="height:250px"></canvas>
-            </div>
-            <!-- /.box-body -->
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="box box-danger">
-            <div class="box-header with-border">
-              <h3 class="box-title">Gastos con categorias</h3>
-
-              <div class="box-tools pull-right">
-                <!--
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                -->
-              </div>
-            </div>
-            <div class="box-body">
-              <canvas id="pieChart2" style="height:250px"></canvas>
-            </div>
-            <!-- /.box-body -->
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="box box-primary">
-            <div class="box-header with-border">
-              <h3 class="box-title">Ingresos vs Gastos</h3>
-
-              <div class="box-tools pull-right">
-                <!--
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>-->
-              </div>
-            </div>
-            <div class="box-body">
-              <div class="chart">
-                <canvas id="areaChart3" style="height:250px"></canvas>
-              </div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-        </div>
       </div>
       <!-- /.row -->
       
