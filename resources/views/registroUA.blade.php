381014b854a2502abb0e7f68e135768f9ec4cf66
eduardo14 <eduardo0091400914@gmail.com>
2017-07-27 01:41:43 -0600

    add users admin

--- resources/views/registroUA.blade.php
+++ resources/views/registroUA.blade.php
@@ -22,7 +22,7 @@
                     <label for="inputName" class="col-sm-2 control-label">Nombre</label>
 
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputName" name="nombre" placeholder="Nombre" required value="{{$user->name}}">
+                      <input type="text" class="form-control" id="inputName" name="nombre" placeholder="Nombre" required >
                     </div>
                   </div>
                   
@@ -30,55 +30,52 @@
                     <label for="inputEmail" class="col-sm-2 control-label">Correo</label>
 
                     <div class="col-sm-10">
-                      <input type="email" class="form-control" id="inputEmail" name="email" placeholder="correo" required value ="{{$user->email}}">
+                      <input type="email" class="form-control" id="inputEmail" name="email" placeholder="correo" required >
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="inputName" class="col-sm-2 control-label">Edad</label>
 
                     <div class="col-sm-10">
-                      <input type="number" class="form-control" id="inputName" name="edad" placeholder="Edad" required value="{{$user->usu_age}}">
+                      <input type="number" class="form-control" id="inputName" name="edad" placeholder="Edad" required >
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="inputExperience" class="col-sm-2 control-label">Profesion</label>
 
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputExperience" name="ocupacion" placeholder="Profesion" required value="{{$user->usu_occupation}}">
+                      <input type="text" class="form-control" id="inputExperience" name="ocupacion" placeholder="Profesion" required >
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="inputoaddress" class="col-sm-2 control-label">Direccion</label>
 
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputoaddress" name="Direccion" placeholder="Direccion" required value="{{$user->usu_address}}">
+                      <input type="text" class="form-control" id="inputoaddress" name="Direccion" placeholder="Direccion" required >
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="inputcity" class="col-sm-2 control-label">Ciudad</label>
 
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputcity" name="Ciudad" placeholder="Ciudad" required value="{{$user->usu_city}}">
+                      <input type="text" class="form-control" id="inputcity" name="Ciudad" placeholder="Ciudad" required >
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="inputstate" class="col-sm-2 control-label">Estado</label>
 
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputstate" name="Estado" placeholder="Estado" required value="{{$user->usu_state}}">
+                      <input type="text" class="form-control" id="inputstate" name="Estado" placeholder="Estado" required >
                     </div>
                   </div>
                   <div class="form-group">   
                     <label class="col-sm-2 control-label" for="sexo">Sexo:</label> 
                     <div class="col-sm-10">
                       <select class="form-control"  name="sexo">
-                       @if($user->usu_sex==0)
-                       <option value="0" selected="">Femenino</option>
-                       <option value="1" >Masculino</option>
-                        @else
+                       
                        <option value="0" >Femenino</option>
                        <option value="1" selected="">Masculino</option>
-                       @endif
+                       
                       </select>
                     </div>
                   </div>
@@ -86,13 +83,10 @@
                     <label class="col-sm-2 control-label" for="tipo">Tipo:</label> 
                     <div class="col-sm-10">
                       <select class="form-control"  name="tipo">
-                       @if($user->usu_type==0)
+                       
                        <option value="0" selected="">Estandar</option>
                        <option value="1" >Administrador</option>
-                        @else
-                       <option value="0" >Estandar</option>
-                       <option value="1" selected="">Administrador</option>
-                       @endif
+                        
                       </select>
                     </div>
                   </div>
